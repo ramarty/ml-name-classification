60642ec978aef2beaa99d6324a8f69a5085d85af
Rob Marty <ramarty@email.wm.edu>
2019-06-15 13:47:29 -0400

    clean data script

--- Code/01_clean_data.R
+++ Code/01_clean_data.R
@@ -1,29 +1,67 @@
 # Ethnicity and Religion Classification
 
-# TODO:
-  # 1. For smaller groups, categorize as "other"
+# Load and Prep Training Data --------------------------------------------------
+data_training <- read.csv(file.path(raw_data_file_path, "training_data_011719.csv"))
 
-# Setup ------------------------------------------------------------------------
-# Filepaths
-project_file_path <- "~/Dropbox/World Bank/Side Work/Names to Ethnicity Classification"
+data_training$d10 <- data_training$d10 %>% as.character %>% iconv("WINDOWS-1252","UTF-8") %>% tolower
+data_training$name <- data_training$name %>% as.character %>% iconv("WINDOWS-1252","UTF-8") %>% tolower
+data_training$sr_ethnicity <- data_training$sr_ethnicity %>% as.character %>% iconv("WINDOWS-1252","UTF-8") %>% tolower
 
-TRAIN_PROP <- 0.75
+data_training <- data_training[data_training$d10 != "",]
+data_training <- data_training[data_training$name != "",]
 
-library(dplyr)
-library(tm)
-library(quanteda)
+# Load and Prep Target Data ----------------------------------------------------
+data_target <- read.csv(file.path(raw_data_file_path, "target_data_011719.csv"))
 
-set.seed(42)
+data_target$mtaa <- data_target$mtaa %>% as.character %>% iconv("WINDOWS-1252","UTF-8") %>% tolower
+data_target$name <- data_target$name %>% as.character %>% iconv("WINDOWS-1252","UTF-8") %>% tolower
+data_target$kata <- data_target$kata %>% as.character %>% iconv("WINDOWS-1252","UTF-8") %>% tolower
 
-# Load and Prep Data -----------------------------------------------------------
-data <- read.csv(file.path(project_file_path, "data", "training_data_011719.csv"))
+# Load and Prep Ethnicity Table ------------------------------------------------
+eth_table <- read.csv(file.path(raw_data_file_path, "ethtable.csv")) %>% 
+  select(c(-F,-Real.Name)) %>%
+  dplyr::rename(sr_ethnicity = Survey.Name)
+names(eth_table) <- names(eth_table) %>% tolower
 
-# Clean Dependent Variable
-data$d10 <- data$d10 %>% iconv("WINDOWS-1252","UTF-8") %>% tolower
-data$name <- data$name %>% iconv("WINDOWS-1252","UTF-8") %>% tolower
+for(var in names(eth_table)){
+  eth_table[[var]] <- eth_table[[var]] %>% as.character %>% iconv("WINDOWS-1252","UTF-8") %>% tolower
+}
 
-data <- data[data$d10 != "",]
-data <- data[data$name != "",]
+## Fill in blanks
+  # If d_i is blank, use d_{i-1}
+for(i in 2:11){
+  eth_table[[paste0("d",i)]][eth_table[[paste0("d",i)]] == ""] <- eth_table[[paste0("d",i-1)]][eth_table[[paste0("d",i)]] == ""]
+}
+
+# Merge Data with Ethnicity Table ----------------------------------------------
+data_training$d10 <- NULL
+data_training <- merge(data_training, eth_table, by="sr_ethnicity", all.x=T, all.y=F)
+data_training <- data_training[!is.na(data_training$d1),] # TODO: Some d groups didn't match
+
+# Export -----------------------------------------------------------------------
+write.csv(data_training, file.path(final_data_file_path, "training_data_011719_clean.csv"), row.names=F)
+saveRDS(data_training, file.path(final_data_file_path, "training_data_011719_clean.Rds"))
+
+write.csv(data_target, file.path(final_data_file_path, "target_data_011719_clean.csv"), row.names=F)
+saveRDS(data_target, file.path(final_data_file_path, "target_data_011719_clean.Rds"))
+
+
+
+# SEND TO FINAL DATA
+
+
+
+
+
+data$d10[!(data$d10 %in% eth_table$d10)] %>% table
+eth_table$d10[!(eth_table$d10 %in% data$d10)] %>% table
+
+
+eth_table$D10 <- eth_table$D10 %>% as.character %>% iconv("WINDOWS-1252","UTF-8") %>% tolower
+
+
+
+
 
 # Usefu Functions --------------------------------------------------------------
 capitalize_firstlast_charword <- function(s){
--- Code/master.R
+++ Code/master.R
@@ -2,8 +2,8 @@
 # Master File
 
 # Filepaths --------------------------------------------------------------------
-if(Sys.info()["user"] == "WB521633") project_file_path <- "C:/Users/wb521633/Dropbox/World Bank/Side Work/Names to Ethnicity Classification"
-if(Sys.info()["user"] == "robmarty") project_file_path <- "~/Dropbox/World Bank/Side Work/Names to Ethnicity Classification"
+if(Sys.info()["user"] == "WB521633") project_file_path <- "C:/Users/wb521633/Dropbox/World Bank/Side Work/ml-name-classification"
+if(Sys.info()["user"] == "robmarty") project_file_path <- "~/Dropbox/World Bank/Side Work/ml-name-classification"
 
 raw_data_file_path <- file.path(project_file_path, "data", "rawdata")
 final_data_file_path <- file.path(project_file_path, "data", "finaldata")
