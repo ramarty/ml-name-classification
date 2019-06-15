52025e9a9630e00c197f7e6978855e7c6b6ed2e5
Rob Marty <ramarty@email.wm.edu>
2019-06-15 09:55:45 -0400

    add master

---
+++ Code/master.R
@@ -0,0 +1,16 @@
+# Names to Ethnicity Classification
+# Master File
+
+# Filepaths --------------------------------------------------------------------
+if(Sys.info()["user"] == "WB521633") project_file_path <- "C:/Users/wb521633/Dropbox/World Bank/Side Work/Names to Ethnicity Classification"
+if(Sys.info()["user"] == "robmarty") project_file_path <- "~/Dropbox/World Bank/Side Work/Names to Ethnicity Classification"
+
+raw_data_file_path <- file.path(project_file_path, "data", "rawdata")
+final_data_file_path <- file.path(project_file_path, "data", "finaldata")
+figures_file_path <- file.path(project_file_path, "Results", "figures")
+tables_file_path <- file.path(project_file_path, "Results", "tables")
+
+# Packages ---------------------------------------------------------------------
+library(quanteda)
+
+
